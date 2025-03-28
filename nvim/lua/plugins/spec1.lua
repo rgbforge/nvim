bfb3d783686ac23dbdc0d3b11ea3b40dbd3c5bb9
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-03-28 13:10:51 -0500

    Update spec1.lua

--- nvim/lua/plugins/spec1.lua
+++ nvim/lua/plugins/spec1.lua
@@ -27,29 +27,4 @@ return {
     require("vim-be-good").menu{} 
   end,
   },
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,  -- Load during startup
-    priority = 1000,  -- High priority to load before other plugins
-    config = function()
-      local c = require('vscode.colors').get_colors()
-      require('vscode').setup({
-        -- Your existing configuration
-        transparent = true,
-        italic_comments = true,
-        underline_links = true,
-        disable_nvimtree_bg = true,
-        terminal_colors = true,
-        color_overrides = {
-          vscLineNumber = '#FFFFFF',
-        },
-        group_overrides = {
-          Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-        }
-      })
-      
-      -- Apply the colorscheme
-      vim.cmd.colorscheme "vscode"
-    end,
-  },
 }
