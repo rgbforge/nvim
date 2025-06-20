6416f210aa424c14562ff656b1cacb97efeca2ca
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-20 11:53:25 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,13 @@
-#git clone https://github.com/neovim/neovim
+cd $HOME/.config/nvim/
+mkdir INSTALL
+cd INSTALL
+git clone https://github.com/neovim/neovim
 cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build
-mkdir ../INSTALL
-cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/../INSTALL ..
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/.config/nvim/INSTALL ..
 make install
-echo 'export PATH="$PATH:$HOME/gdir/neovim/INSTALL/bin"' >> ~/.bashrc
-echo 'export XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/gdir/neovim/INSTALL/share"' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/gdir/neovim/INSTALL/lib64"' >> ~/.bashrc
+echo 'export PATH="$PATH:$HOME/.config/nvim/INSTALL/bin"' >> ~/.bashrc
+echo 'export XDG_DATA_DIRS="$XDG_DATA_DIRS:$HOME/.config/nvim/INSTALL/share"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.config/nvim/INSTALL/lib64"' >> ~/.bashrc
 echo "alias n='nvim'" >> ~/.bashrc
