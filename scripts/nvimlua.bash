40d37f119dc84ca91ad1ff2c8b01a7302ae5354a
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 14:44:05 -0500

    Update nvimlua.bash

--- scripts/nvimlua.bash
+++ scripts/nvimlua.bash
@@ -1,8 +1,5 @@
 sudo apt install luajit libluajit-5.1-dev lua-lpeg libunibilium-dev lua-luv-dev gettext liblua5.1-dev lua5.1
 
-sudo apt install lua
-sudo apt install lua5.4
-sudo apt install luv
 sudo apt install lua-luv
 sudo apt install lua-luv-dev
 sudo apt install lua-lpeg-dev
@@ -16,18 +13,7 @@ sudo apt install unibilium
 sudo apt install libunibilium-dev
 sudo apt install libutf8-dev
 sudo apt install libutf8proc-de
-sudo apt install lua-jit-dev
-sudo apt install luajit-dev
-sudo apt install luajit
-sudo apt install libluajit-dev
-sudo apt install libluajit
-sudo apt install luajit
-sudo apt install libluajit
-sudo apt install luajit
-sudo apt install luajit-dev
-sudo apt install libluajit-5
-sudo apt install libluajit-5.1-2
-sudo apt install libluajit-5.1-2-dev
+
 sudo apt install luajit
 sudo apt install libluajit-5.1-dev
 sudo apt install libgettext-dev
