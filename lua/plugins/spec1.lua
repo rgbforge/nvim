4056725474a9c0cea3607ad6add1e138ca024ccd
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 13:23:11 -0500

    Update spec1.lua

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -1,19 +1,19 @@
 return {
-  { 
-    "nvim-tree/nvim-web-devicons", 
-    lazy = true 
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
   },
-  {  
+  {
     "ThePrimeagen/vim-be-good",
-    cmd = "VimBeGood",  
+    cmd = "VimBeGood",
     config = function()
-      require("vim-be-good").menu{} 
+      require("vim-be-good").setup({})
     end,
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000
+    priority = 1000,
   },
   {
     "folke/tokyonight.nvim",
@@ -22,10 +22,25 @@ return {
     opts = {},
   },
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    branch = 'main',
-    build = ':TSUpdate'
+    branch = "main",
+    build = ":TSUpdate",
+  },
+  {
+    "nvim-lua/plenary.nvim",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+    },
   },
   {
     "p00f/clangd_extensions.nvim",
@@ -55,14 +70,49 @@ return {
     },
   },
   {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "clangd", "pyright", "ruff_lsp" },
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = { "p00f/clangd_extensions.nvim" },
     config = function()
       local lspconfig = require("lspconfig")
+      local vim = vim
+
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+      end
+
+      local on_attach_ruff = function(client, bufnr)
+        client.server_capabilities.hoverProvider = false
+        on_attach(client, bufnr)
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "<leader>co", function()
+          vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+        end, bufopts)
+      end
+
       lspconfig.clangd.setup({
-        keys = {
-          { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
-        },
+        on_attach = on_attach,
+        keys = { { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" } },
         root_dir = function(fname)
           return require("lspconfig.util").root_pattern(
             "Makefile",
@@ -72,13 +122,14 @@ return {
             "meson.build",
             "meson_options.txt",
             "build.ninja"
-          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
-            fname
-          ) or require("lspconfig.util").find_git_ancestor(fname)
+          )(fname)
+            or require("lspconfig.util").root_pattern(
+              "compile_commands.json",
+              "compile_flags.txt"
+            )(fname)
+            or require("lspconfig.util").find_git_ancestor(fname)
         end,
-        capabilities = {
-          offsetEncoding = { "utf-16" },
-        },
+        capabilities = { offsetEncoding = { "utf-16" } },
         cmd = {
           "clangd",
           "--background-index",
@@ -88,27 +139,11 @@ return {
           "--function-arg-placeholders",
           "--fallback-style=llvm",
         },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
+        init_options = { usePlaceholders = true, completeUnimported = true, clangdFileStatus = true },
       })
+
+      lspconfig.pyright.setup({ on_attach = on_attach })
+      lspconfig.ruff_lsp.setup({ on_attach = on_attach_ruff })
     end,
   },
-  {
-    "nvim-lua/plenary.nvim",
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",        
-      "sindrets/diffview.nvim",
-    
-    }
-  }
 }
