604caddf8854747926ae0a41398be2b758c0f402
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 12:52:58 -0500

    Update spec1.lua

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -3,28 +3,12 @@ return {
     "nvim-tree/nvim-web-devicons", 
     lazy = true 
   },
-  {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime",
-    init = function()
-      vim.g.startuptime_tries = 10
-    end,
-  },
   {  
     "ThePrimeagen/vim-be-good",
     cmd = "VimBeGood",  
     config = function()
       require("vim-be-good").menu{} 
     end,
-  },
-  {
-    "nvim-neorg/neorg",
-    ft = "norg",
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-      },
-    },
   },
   {
     "catppuccin/nvim",
@@ -42,5 +26,78 @@ return {
   lazy = false,
   branch = 'main',
   build = ':TSUpdate'
+  },
+  {
+  "p00f/clangd_extensions.nvim",
+  opts = {
+    inlay_hints = {
+      inline = false,
+    },
+    ast = {
+      role_icons = {
+        type = "",
+        declaration = "",
+        expression = "",
+        specifier = "",
+        statement = "",
+        ["template argument"] = "",
+      },
+      kind_icons = {
+        Compound = "",
+        Recovery = "",
+        TranslationUnit = "",
+        PackExpansion = "",
+        TemplateTypeParm = "",
+        TemplateTemplateParm = "",
+        TemplateParamObject = "",
+      },
+    },
+  },
+},
+{
+  "neovim/nvim-lspconfig",
+  dependencies = { "p00f/clangd_extensions.nvim" },
+  config = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.clangd.setup({
+      keys = {
+        { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+      },
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+          fname
+        ) or require("lspconfig.util").find_git_ancestor(fname)
+      end,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+      },
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+    })
+  end,
+},
+   {
+  "nvim-lua/plenary.nvim",
   }
+
 }
