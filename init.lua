a28eb17ff3c94ffb829bf9828a29796994dcbe37
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 14:24:36 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,22 @@
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.opt.clipboard = "unnamedplus"
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+require("config.lsp")
 require("config.lazy")
+
 vim.cmd.colorscheme "catppuccin"
