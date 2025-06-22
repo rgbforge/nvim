c40be0460710b8082c909998d0f5e27bbae65cd9
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 14:42:08 -0500

    Update nvimlua.bash

--- scripts/nvimlua.bash
+++ scripts/nvimlua.bash
@@ -1,1 +1,49 @@
 sudo apt install luajit libluajit-5.1-dev lua-lpeg libunibilium-dev lua-luv-dev gettext liblua5.1-dev lua5.1
+
+sudo apt install lua
+sudo apt install lua5.4
+sudo apt install luv
+sudo apt install lua-luv
+sudo apt install lua-luv-dev
+sudo apt install lua-lpeg-dev
+sudo apt install tree-sitter-dev
+sudo apt install tree-sitter
+sudo apt install libtree-sitter-dev
+sudo apt install libunibilium
+sudo apt install libunib
+sudo apt install libuni
+sudo apt install unibilium
+sudo apt install libunibilium-dev
+sudo apt install libutf8-dev
+sudo apt install libutf8proc-de
+sudo apt install lua-jit-dev
+sudo apt install luajit-dev
+sudo apt install luajit
+sudo apt install libluajit-dev
+sudo apt install libluajit
+sudo apt install luajit
+sudo apt install libluajit
+sudo apt install luajit
+sudo apt install luajit-dev
+sudo apt install libluajit-5
+sudo apt install libluajit-5.1-2
+sudo apt install libluajit-5.1-2-dev
+sudo apt install luajit
+sudo apt install libluajit-5.1-dev
+sudo apt install libgettext-dev
+sudo apt install libgettext
+sudo apt install gettext
+sudo apt install gettext-dev
+
+sudo apt install lua5.1 liblua5.1-0-dev
+sudo apt-get install luajit
+sudo apt-get install luajit-dev
+sudo apt-get install libluajit5.1
+
+sudo apt remove lua5.4
+sudo apt install lua5.1
+sudo apt install lua5.1-dev
+sudo apt install liblua5.1-dev
+sudo apt remove lua5.1 liblua5.1-0-dev
+sudo apt-get remove luajit
+sudo apt remove libluajit-5.1-dev
