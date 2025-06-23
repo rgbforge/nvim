ada661abd00ea16b4f07b0f911bdc382f8c94880
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 21:58:29 -0500

    Update list.lua

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -33,96 +33,9 @@ return {
           "lua",
           "python",
     	},
-
     	highlight = {
       	  enable = true,
     	},
-
      },
-  },
-  {
-    "nvim-lua/plenary.nvim",
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "sindrets/diffview.nvim",
-    },
-  },
-  {
-    "p00f/clangd_extensions.nvim",
-    opts = {
-      inlay_hints = {
-        inline = false,
-      },
-      ast = {
-        role_icons = {
-          type = "",
-          declaration = "",
-          expression = "",
-          specifier = "",
-          statement = "",
-          ["template argument"] = "",
-        },
-        kind_icons = {
-          Compound = "",
-          Recovery = "",
-          TranslationUnit = "",
-          PackExpansion = "",
-          TemplateTypeParm = "",
-          TemplateTemplateParm = "",
-          TemplateParamObject = "",
-        },
-      },
-    },
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "pyright", "ruff" },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "p00f/clangd_extensions.nvim" },
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',  
-      'hrsh7th/cmp-path',
-    },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'buffer' },
-          { name = 'path' },
-        })
-      })
-    end
   },
 }
