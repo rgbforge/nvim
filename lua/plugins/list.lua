7f667793ad07f4c9faaac8d0ca20dce7c53c34ba
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-06-22 21:47:48 -0500

    Update list.lua

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -100,4 +100,29 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "p00f/clangd_extensions.nvim" },
   },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',  
+      'hrsh7th/cmp-path',
+    },
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+    end
+  },
 }
