fe56238cba0ec1c571620c2f63275af15a9a54b2
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-07-28 13:47:52 -0500

    Delete scripts/brew.zsh

--- scripts/brew.zsh
+++
@@ -1,5 +0,0 @@
-brew install ninja libtool automake cmake pkg-config gettext curl
-#git clone https://github.com/neovim/neovim.git
-#cd neovim
-#make CMAKE_BUILD_TYPE=RelWithDebInfo
-#sudo make install
