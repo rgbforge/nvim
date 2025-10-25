e306c3df7947cc35474489390d840a3a638deddc
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-10-25 12:57:06 -0500

    Update list.lua

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -44,7 +44,6 @@ return {
     },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
 
       local on_attach = function(client, bufnr)
         vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -66,24 +65,9 @@ return {
           end, bufopts)
         end
       end
-
-      lspconfig.clangd.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          pyright = { disableOrganizeImports = true },
-        },
-      })
-
-      lspconfig.ruff.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
+    --vim.lsp.enable('pyright')
+    vim.lsp.enable('clangd')
+      
     end,
   },
   {
