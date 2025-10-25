4195d719d277db64aea06843b05980f9754c5206
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-10-25 12:45:07 -0500

    Update clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,1 +1,14 @@
-rm -rf $HOME/.local/share/nvim $HOME/.local/state/nvim $HOME/.cache/nvim
+rm -rf $HOME/.local/share/nvim 
+#~/.local/share/nvim/lazy/
+#catppuccin              diffview.nvim         mason.nvim  nvim-lspconfig     plenary.nvim     vim-be-good
+#clangd_extensions.nvim  lazy.nvim             neogit      nvim-treesitter    telescope.nvim   vscode.nvim
+#cmp-nvim-lsp            mason-lspconfig.nvim  nvim-cmp    nvim-web-devicons  tokyonight.nvim
+
+#~/.local/share/nvim/mason/
+#bin  packages  registries  share  staging
+
+rm -rf $HOME/.local/state/nvim
+#lazy  log  lsp.log  mason.log  shada  swap
+
+rm -rf $HOME/.cache/nvim
+#luac  mason-registry-update
