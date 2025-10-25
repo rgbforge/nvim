288a25da2bf7ad3421beabcd5744bdaa1af15292
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-10-25 12:50:33 -0500

    Update list.lua

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -42,7 +42,68 @@ return {
       "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
     },
-  },   
-  
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      local on_attach = function(client, bufnr)
+        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+        local bufopts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+
+        if client.name == "ruff" then
+          client.server_capabilities.hoverProvider = false
+          vim.keymap.set("n", "<leader>co", function()
+            vim.lsp.buf.code_action({
+              context = { only = { "source.organizeImports" } },
+              apply = true,
+            })
+          end, bufopts)
+        end
+      end
+
+      lspconfig.clangd.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          pyright = { disableOrganizeImports = true },
+        },
+      })
+
+      lspconfig.ruff.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" },
+    config = function()
+      vim.o.pumheight = 2
+      local cmp = require("cmp")
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
   { "p00f/clangd_extensions.nvim" },
 }
