2949bfc84701ec74a1fce2ef779c990256a72ab8
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-10-25 13:52:44 -0500

    Update list.lua

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -1,95 +1,32 @@
-return {
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
-  { "Mofiqul/vscode.nvim" },
-  { "ThePrimeagen/vim-be-good", cmd = "VimBeGood" },
-  { "nvim-lua/plenary.nvim" },
+vim.pack.add({
+  --{
+    --src = 'https://github.com/user/plugin3',
+    -- -- Version constraint, see |vim.version.range()|
+    --version = vim.version.range('1.0'),
+    -- -- Git branch, tag, or commit hash
+    --version = 'main',
+  --},
+-- Plugin's code can be used directly after `add()`
+--plugin1 = require('plugin1')
+  {src = 'nvim-tree/nvim-web-devicons', lazy = true },
+  {src = 'catppuccin/nvim', name = "catppuccin", priority = 1000 },
+  {src = 'folke/tokyonight.nvim', lazy = false, priority = 1000, opts = {} },
+  {src = 'Mofiqul/vscode.nvim' },
+  {src = 'ThePrimeagen/vim-be-good', cmd = "VimBeGood" },
+  {src = 'nvim-lua/plenary.nvim'},
   {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    src = 'nvim-telescope/telescope.nvim',
+    version = '0.1.8',
   },
   {
-    "NeogitOrg/neogit",
-    dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+   src = 'NeogitOrg/neogit',
   },
   {
-    "nvim-treesitter/nvim-treesitter",
+   src = 'nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     opts = {
       ensure_installed = { "c", "cpp", "lua", "python" },
       highlight = { enable = true },
     },
   },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "clangd", "pyright", "ruff" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "p00f/clangd_extensions.nvim",
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      local on_attach = function(client, bufnr)
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
-
-        if client.name == "ruff" then
-          client.server_capabilities.hoverProvider = false
-          vim.keymap.set("n", "<leader>co", function()
-            vim.lsp.buf.code_action({
-              context = { only = { "source.organizeImports" } },
-              apply = true,
-            })
-          end, bufopts)
-        end
-      end
-    vim.lsp.enable('clangd')
-    vim.lsp.enable('pyright')
-    vim.lsp.enable('ruff')
-    
-      
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" },
-    config = function()
-      vim.o.pumheight = 2
-      local cmp = require("cmp")
-      cmp.setup({
-        mapping = cmp.mapping.preset.insert({
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-          ["<C-Space>"] = cmp.mapping.complete(),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
-  },
-  { "p00f/clangd_extensions.nvim" },
-}
+})
