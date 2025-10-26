3b66b025510f3866a93d9ad22f8f1464851bb5b0
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-10-25 21:24:37 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -16,14 +16,8 @@ for type, icon in pairs(signs) do
 end
 
 vim.pack.add({
-  --{src = 'https://github.com/nvim-tree/nvim-web-devicons', lazy = true },
-  --{src = 'https://github.com/catppuccin/nvim', name = "catppuccin", priority = 1000 },
-  --{src = 'https://github.com/folke/tokyonight.nvim', lazy = false, priority = 1000, opts = {} },
   {src = 'https://github.com/Mofiqul/vscode.nvim' },
-  --{src = 'https://github.com/ThePrimeagen/vim-be-good', cmd = "VimBeGood" },
   {src = 'https://github.com/nvim-lua/plenary.nvim'},
-  --{src = 'https://github.com/nvim-telescope/telescope.nvim', version = '0.1.8',},
-  --{src = 'https://github.com/NeogitOrg/neogit'},
   {src = 'https://github.com/nvim-treesitter/nvim-treesitter',
     build = ":TSUpdate",
     opts = {  ensure_installed = { "c", "cpp", "lua", "python" },nhighlight = { enable = true },
@@ -65,8 +59,6 @@ require('vscode').setup({
 })
 
 vim.cmd.colorscheme "vscode"
---vim.cmd.colorscheme "catppuccin"
---vim.cmd[[colorscheme tokyonight-storm]]
 vim.lsp.enable('pyright')
 vim.lsp.enable('clangd')
 vim.lsp.enable('ruff')
